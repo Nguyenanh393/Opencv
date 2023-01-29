0c9adbd95637cec38f6abb005d836e56a63cbbc4
Nguyenanh393 <93720800+Nguyenanh393@users.noreply.github.com>
2023-01-29 18:27:35 +0700

    Update RW-img.py

--- Learn-OpenCV/RW-img/RW-img.py
+++ Learn-OpenCV/RW-img/RW-img.py
@@ -11,3 +11,4 @@ cv2.imshow("unchanged", img_unchanged)
 cv2.waitKey(0) #cố định cửa sổ win 
 
 cv2.imwrite("grayscale.jpg", img_grayscale)
+#https://pixabay.com/vi/videos/ch%C3%B3-s%C3%B3i-th%C3%BA-v%E1%BA%ADt-%C4%91%E1%BB%99ng-v%E1%BA%ADt-c%C3%B3-v%C3%BA-27400/
